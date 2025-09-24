734f82bc0d7e4b4cc69b64c129cbbc76949cc476
Anna Pohorila <annapohorila07@gmail.com>
2025-09-24 20:30:06 +0300

    До структури Point додано конструктори та перевантажено оператори зчитування та запису точки.

--- lab1.h
+++ lab1.h
@@ -9,14 +9,25 @@ struct Point {
 	double x = 0;
 	double y = 0;
 
+	Point() = default;
+	Point(double x, double y) : x(x), y(y){}
+
 	Point operator+(const Point& other) const {
 		return (x + other.x, y + other.y);
 	}
 
-	Point operator/(double number) {
+	Point operator/(double number) const{
 		return (x / number, y / number);
 	}
 };
+
+std::ostream& operator<<(std::ostream& os, Point const& p) {
+	return os << p.x << " " << p.y;
+}
+
+std::istream& operator>>(std::istream& is, Point& p) {
+	return is >> p.x >> p.y;
+}
 
 class PointGeneration {
 private: 
