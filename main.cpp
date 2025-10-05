151b31eb3e12266425d9dd9e5d3398b81ec3f998
Anna Pohorila <annapohorila07@gmail.com>
2025-10-05 16:42:02 +0300

    Додано обгортку try/catch

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #include "pointgenerator.h"
 
 int main() {
+    try {
     std::string input_file = "input.txt";
 	std::string output_file = "output.txt";
+    
 	int n;
 	Point initial_point;
 	std::vector<Point> base_points;
 	read_from_file(input_file, n, initial_point, base_points);
-
 
 	PointGeneration generator(initial_point, base_points);
 	std::vector<Point> output_points;
@@ -20,5 +21,10 @@ int main() {
 
 	write_in_file(output_file, output_points);
 
+} catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    }
 
+    return 0;
 }
